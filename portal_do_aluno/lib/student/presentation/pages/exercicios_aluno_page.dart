f50c4a4fc04dbdba71493029174eb48a0f4c9069
Ian Page Miranda Maciel <ianpage27@gmail.com>
2025-11-17 17:35:34 -0300

    página modelo com estrutura base

--- portal_do_aluno/lib/student/presentation/pages/exercicios_aluno_page.dart
+++ portal_do_aluno/lib/student/presentation/pages/exercicios_aluno_page.dart
@@ -1,11 +1,13 @@
 // --- PAGINA TESTE --- //
 
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:portal_do_aluno/admin/presentation/providers/user_provider.dart';
 import 'package:portal_do_aluno/admin/helper/snack_bar_personalizado.dart';
 import 'package:portal_do_aluno/navigation/route_names.dart';
 import 'package:portal_do_aluno/shared/widgets/app_bar.dart';
+import 'package:portal_do_aluno/student/presentation/pages/exercicios_detalhes_page.dart';
 import 'package:provider/provider.dart';
 
 class ExerciciosAlunoPage extends StatefulWidget {
@@ -19,11 +21,33 @@ class _ExerciciosAlunoPageState extends State<ExerciciosAlunoPage> {
   String? turmaId;
   bool showModal = false;
 
-  Stream<QuerySnapshot> getExerciciosPorTurma(turmaId) {
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    if (turmaId == null) {
+      buscarAlunoPorTurma();
+    }
+  }
+
+  Stream<QuerySnapshot> getExerciciosPorTurma(String turmaId) {
     return FirebaseFirestore.instance
         .collection('exercicios')
         .where('turmaId', isEqualTo: turmaId)
         .snapshots();
+  }
+
+  void buscarAlunoPorTurma() async {
+    final userId = Provider.of<UserProvider>(context).userId;
+
+    final snapshort = await FirebaseFirestore.instance
+        .collection('usuarios')
+        .doc(userId)
+        .get();
+    debugPrint(userId);
+    debugPrint(snapshort.data().toString());
+    setState(() {
+      turmaId = snapshort.data()?['turmaId'];
+    });
   }
 
   String limitarTexto(String texto, int limite) {
@@ -38,8 +62,9 @@ class _ExerciciosAlunoPageState extends State<ExerciciosAlunoPage> {
 
   @override
   Widget build(BuildContext context) {
-    final userId = Provider.of<UserProvider>(context).userId;
-    
+    if (turmaId == null) {
+      return const Center(child: CircularProgressIndicator());
+    }
     return Scaffold(
       appBar: const CustomAppBar(
         title: 'Exercícios',
@@ -48,127 +73,122 @@ class _ExerciciosAlunoPageState extends State<ExerciciosAlunoPage> {
       body: Padding(
         padding: const EdgeInsets.all(12),
         child: StreamBuilder<QuerySnapshot>(
-          stream: getExerciciosPorTurma(turmaId),
+          stream: getExerciciosPorTurma(turmaId!),
           builder: (context, snapshot) {
             final data = snapshot.data?.docs;
 
             if (snapshot.connectionState == ConnectionState.waiting) {
               return const Center(child: CircularProgressIndicator());
             }
-            return AnimatedContainer(
-              duration: const Duration(seconds: 1),
-              curve: Curves.easeInOut,
-              child: ListView.builder(
-                itemCount: data?.length,
-                itemBuilder: (context, index) {
-                  final exercicio = data?[index];
-                  final titulo = exercicio?['titulo'];
-                  final conteudoDoExercicio = exercicio?['conteudoDoExercicio'];
+            return ListView.builder(
+              itemCount: data?.length,
+              itemBuilder: (context, index) {
+                final exercicio = data?[index];
+                final titulo = exercicio?['titulo'];
+                final conteudoDoExercicio = exercicio?['conteudoDoExercicio'];
 
-                  return Card(
-                    child: ListTile(
-                      title: Text(titulo),
-                      subtitle: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(limitarTexto(conteudoDoExercicio, 10)),
-                          const SizedBox(height: 8),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: [
-                              AnimatedContainer(
-                                duration: const Duration(seconds: 1),
-                                curve: Curves.easeInOut,
-                                child: IconButton(
-                                  onPressed: () {
-                                    showModalBottomSheet(
-                                      context: context,
-                                      builder: (context) {
-                                        return Column(
-                                          children: [
-                                            ListTile(
-                                              title: const Text('Instruções'),
-                                              subtitle: Text(
-                                                conteudoDoExercicio,
-                                              ),
-                                            ),
-                                            const Divider(),
-                                            Padding(
-                                              padding:
-                                                  const EdgeInsets.symmetric(
-                                                    horizontal: 16,
-                                                    vertical: 8,
-                                                  ),
-                                              child: Row(
-                                                mainAxisAlignment:
-                                                    MainAxisAlignment
-                                                        .spaceBetween,
-                                                children: [
-                                                  ElevatedButton.icon(
-                                                    onPressed: () {
-                                                      snackBarPersonalizado(
-                                                        context: context,
-                                                        mensagem:
-                                                            'Arquivo anexado com sucesso',
-                                                        cor: Colors.green,
-                                                      );
-                                                    },
-                                                    label: const Padding(
-                                                      padding:
-                                                          EdgeInsets.symmetric(
-                                                            horizontal: 16,
-                                                          ),
-                                                      child: Text(
-                                                        'Anexar arquivo',
-                                                      ),
-                                                    ),
-                                                  ),
-                                                  ElevatedButton.icon(
-                                                    onPressed: () {
-                                                      snackBarPersonalizado(
-                                                        context: context,
-                                                        mensagem:
-                                                            'Exercício concluído com sucesso!',
-                                                        cor: Colors.green,
-                                                      );
-                                                    },
-                                                    label: const Padding(
-                                                      padding:
-                                                          EdgeInsets.symmetric(
-                                                            horizontal: 16,
-                                                          ),
-                                                      child: Text(
-                                                        'Enviar Exercicio',
-                                                      ),
-                                                    ),
-                                                    icon: const Icon(
-                                                      Icons.send,
-                                                    ),
-                                                  ),
-                                                ],
-                                              ),
-                                            ),
-                                          ],
-                                        );
-                                      },
-                                    );
-                                  },
-                                  icon: showModal == true
-                                      ? const Icon(Icons.arrow_circle_right_outlined)
-                                      : const  Icon(Icons.arrow_circle_down),
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      PageRouteBuilder(
+                        transitionDuration: const Duration(milliseconds: 400),
+                        pageBuilder: (context, animation, secundaryAnimation) {
+                          return SlideTransition(
+                            position: Tween<Offset>(
+                              begin: const Offset(1, 0),
+                              end: Offset.zero,
+                            ).animate(animation),
+
+                            child: ExerciciosDetalhesPage(
+                              exercicios: exercicio,
+                            ),
+                          );
+                        },
+                      ),
+                    );
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(5),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Hero(
+                            tag: exercicio!.id,
+
+                            child: Material(
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: const Color.fromARGB(
+                                    255,
+                                    109,
+                                    87,
+                                    116,
+                                  ),
+                                  borderRadius: BorderRadius.circular(12),
+                                ),
+                                child: cardExecicio(
+                                  titulo,
+                                  conteudoDoExercicio,
                                 ),
                               ),
-                            ],
+                            ),
                           ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
-                  );
-                },
-              ),
+                  ),
+                );
+              },
             );
           },
         ),
+      ),
+    );
+  }
+
+  Widget cardExecicio(String titulo, String conteudo) {
+    return Container(
+      width: double.infinity,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(12),
+        color: const Color.fromARGB(255, 109, 87, 116),
+      ),
+      height: 70,
+
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8),
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width * 0.75,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Exercicio de: $titulo',
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
+                  Text(limitarTexto(conteudo, 50)),
+                ],
+              ),
+            ),
+          ),
+          Container(
+            height: 70,
+            width: 50,
+            decoration: const BoxDecoration(
+              borderRadius: BorderRadius.only(
+                topRight: Radius.circular(12),
+                bottomRight: Radius.circular(12),
+              ),
+              color: Color.fromARGB(255, 26, 110, 236),
+            ),
+
+            child: const Icon(CupertinoIcons.arrow_right, size: 20),
+          ),
+        ],
       ),
     );
   }
---
+++ portal_do_aluno/lib/student/presentation/pages/exercicios_detalhes_page.dart
@@ -0,0 +1,53 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class ExerciciosDetalhesPage extends StatelessWidget {
+  final QueryDocumentSnapshot exercicios;
+  const ExerciciosDetalhesPage({super.key, required this.exercicios});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Detalhes do Exercício')),
+      body: Center(
+        child: Hero(
+          tag: exercicios.id,
+          child: Material(
+            color: Colors.transparent,
+            child: Padding(
+              padding: const EdgeInsets.all(30),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Theme.of(context).cardColor,
+                  borderRadius: BorderRadius.circular(10),
+                ),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        IconButton(
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                          icon: const Icon(Icons.close),
+                        ),
+                      ],
+                    ),
+                    Text(exercicios['titulo']),
+                    const SizedBox(height: 10),
+                    Expanded(
+                      child: SingleChildScrollView(
+                        child: Text(exercicios['conteudoDoExercicio']),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
