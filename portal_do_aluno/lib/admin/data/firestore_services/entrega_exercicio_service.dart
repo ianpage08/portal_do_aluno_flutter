f6fdc039e8473d20695a3c5be3ce36452307a26a
Ian Page Miranda Maciel <ianpage27@gmail.com>
2025-11-18 13:57:39 -0300

    serviço de envio ao Firestore

---
+++ portal_do_aluno/lib/admin/data/firestore_services/entrega_exercicio_service.dart
@@ -0,0 +1,30 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:portal_do_aluno/admin/data/models/entrega_de_atividade.dart';
+
+final FirebaseFirestore _firestore = FirebaseFirestore.instance;
+
+class EntregaExercicioService {
+  Future<void> entregarExercicio(
+    String exerciciosId,
+    String alunoId,
+    EntregaDeAtividade entrega,
+  ) async {
+    final docRef = _firestore
+        .collection('exercicios')
+        .doc(exerciciosId)
+        .collection('entregas')
+        .doc(alunoId)
+        ;
+
+    return docRef.set(entrega.toJson());
+  }
+
+  Future<void> excluirAtividade(String exerciciosId, String alunoId) async {
+    return _firestore
+        .collection('exercicios')
+        .doc(exerciciosId)
+        .collection('entregas')
+        .doc(alunoId)
+        .delete();
+  }
+}
